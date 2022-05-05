49bbfef1c4ab4376a7280e85a5516b15dd3d181b
Filip Figiel <figiel.filip@gmail.com>
2022-05-05 18:28:10 +0200

    feat: add robot actions

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
+import random
+from typing import List
+
 
 class Time(int):
     """Time is a unit of simulation steps. One step is 100ms"""
 
-    pass
-
-
-class Robot:
     pass
 
 
@@ -31,6 +30,63 @@ class Foobar:
 
 class Money(int):
     pass
+
+
+class Robot:
+    def __init__(self):
+        self.action = RobotActionIdle()
+
+
+class RobotAction:
+    """Base class for all robot actions."""
+
+    pass
+
+
+class RobotActionIdle(RobotAction):
+    """The robot has nothing to do."""
+
+    pass
+
+
+class RobotActionChangingTask(RobotAction):
+    """Moving to change activity: occupy the robot for 5 seconds."""
+
+    def __init__(self, next_task: RobotAction):
+        self.remaining_time = Time(50)
+        self.next_task = next_task
+
+
+class RobotActionMiningFoo(RobotAction):
+    """Mining foo: occupies the robot for 1 second."""
+
+    def __init__(self):
+        self.remaining_time = Time(10)
+
+
+class RobotActionMiningBar(RobotAction):
+    """Mining bar: keeps the robot busy for a random time between 0.5 and 2 seconds."""
+
+    def __init__(self):
+        random_ticks = round(random.random() * 15)
+        self.remaining_time = Time(random_ticks + 5)
+
+
+class RobotActionAssemblingFoobar(RobotAction):
+    """Assembling a foobar from a foo and a bar: keeps the robot busy for 2 seconds."""
+
+    def __init__(self, foo: Foo, bar: Bar):
+        self.foo = foo
+        self.bar = bar
+        self.remaining_time = Time(20)
+
+
+class RobotActionSellingFoobar(RobotAction):
+    """Sell foobar: 10s to sell from 1 to 5 foobar."""
+
+    def __init__(self, foobars: List[Foobar]):
+        self.foobars = foobars
+        self.remaining_time = Time(100)
 
 
 class State:
